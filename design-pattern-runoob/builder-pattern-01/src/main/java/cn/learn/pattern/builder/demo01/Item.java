7b21c62f715f0cc49bd7839b2afbd316afb44709
DESKTOP-FN71FDS\Fly365 <771645231@qq.com>
2018-06-19 16:29:14 +0800

    builder pattern

---
+++ design-pattern-runoob/builder-pattern-01/src/main/java/cn/learn/pattern/builder/demo01/Item.java
@@ -0,0 +1,14 @@
+package cn.learn.pattern.builder.demo01;
+
+/**
+ * design-pattern-runoob-cn.learn.pattern.builder.demo01
+ * @author : WXF
+ * @date : 2018年-06月-18日
+ */
+public interface Item {
+
+    String name();
+    Packing packing();
+    float price();
+
+}
---
+++ design-pattern-runoob/builder-pattern-01/src/main/java/cn/learn/pattern/builder/demo01/Packing.java
@@ -0,0 +1,12 @@
+package cn.learn.pattern.builder.demo01;
+
+/**
+ * design-pattern-runoob-cn.learn.pattern.builder.demo01
+ * @author : WXF
+ * @date : 2018年-06月-18日
+ */
+public interface Packing {
+
+    String pack();
+
+}
